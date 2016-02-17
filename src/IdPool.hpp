662e952afac6ad7a4b82045c5255c54c37a69859
Eirik Igland <eirabben@gmail.com>
2016-02-17 16:34:06 +0100

    Cleanup unneeded stuff

--- src/IdPool.hpp
+++ src/IdPool.hpp
@@ -10,73 +10,27 @@ namespace ecs {
 constexpr int defaultPoolSize {100};
 
 class IdPool {
+    using Index = unsigned int;
 
 public:
-    IdPool() {
-        resize(defaultPoolSize);
-    }
+    IdPool();
+    IdPool(std::size_t capacity);
 
-    IdPool(std::size_t capacity) {
-        resize(capacity);
-    }
+    // Ids
+    Id create();
+    void remove(Id id);
+    Id get(Index index);
+    bool isValid(Id id);
 
-    auto create() {
-        Id id;
-
-        if (free.empty()) {
-            id.index = nextIndex++;
-            id.counter = 1;
-            pool[id.index] = id;
-        } else {
-            unsigned int index = free.back();
-            id = pool[index];
-            free.pop_back();
-        }
-
-        return id;
-    }
-
-    void remove(Id id) {
-        if (isValid(id)) {
-            pool[id.index].counter++;
-            free.push_back(id.index);
-        }
-    }
-
-    auto get(unsigned int index) {
-        assert(index < pool.size());
-        assert(pool[index].counter > 0);
-
-        return pool[index];
-    }
-
-    auto& getActive() {
-        return pool;
-    }
-
-    bool isValid(Id id) {
-        return (id.counter == pool[id.index].counter);
-    }
-
-    auto getCapacity() {
-        return pool.size();
-    }
-
-    void resize(std::size_t capacity) {
-        pool.resize(capacity);
-    }
-
-    void clear() {
-        nextIndex = 0;
-        pool.clear();
-        free.clear();
-        pool.resize(defaultPoolSize);
-    }
+    // Pool
+    std::size_t getCapacity();
+    void resize(std::size_t capacity);
+    void clear();
 
 private:
-    unsigned int nextIndex {0};
+    Index nextIndex {0};
     std::vector<Id> pool;
-    std::vector<unsigned int> free;
+    std::vector<Index> free;
 };
 
 }
